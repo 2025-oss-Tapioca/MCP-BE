ae7a654e8f8f6c086e8a991011686c6c964fa606
top6543top <top6543top@gamil.com>
2025-07-23 17:06:50 +0900

    [feat] Spring Ai이용으로 인한 기존 로직 변경 및 초기 프롬프트 제작

--- src/main/java/com/tapioca/MCPBE/service/GptService.java
+++ src/main/java/com/tapioca/MCPBE/service/GptService.java
@@ -1,51 +1,36 @@
 package com.tapioca.MCPBE.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tapioca.MCPBE.domain.dto.GptRequestDto;
-import com.tapioca.MCPBE.domain.dto.GptResponseDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tapioca.MCPBE.domain.dto.gpt.BeRequestDto;
+import com.tapioca.MCPBE.domain.dto.gpt.GptResponseDto;
+import com.tapioca.MCPBE.exception.CustomException;
+import com.tapioca.MCPBE.exception.ErrorCode;
+import com.tapioca.MCPBE.util.prompt.GptPrompt;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class GptService {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final OpenAiChatModel openAiChatModel;
 
-    @Value("${gpt.api.key}")
-    private String apiKey;
-
-    @Value("${gpt.api.url}")
-    private String apiUrl;
-
-
-    public GptResponseDto gptRequest(GptRequestDto requestDto) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth(apiKey);
-
-        HttpEntity<GptRequestDto> request = new HttpEntity<>(requestDto, headers);
-
-        ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, request, String.class);
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode root = mapper.readTree(response.getBody());
-
-            String content = root.path("choices").get(0).path("message").path("content").asText();
-
-            return new GptResponseDto(content);
+    public GptResponseDto gptRequest(BeRequestDto beRequestDto) {
+        String prompt = makePrompt(beRequestDto.userRequest());
+        System.out.println(prompt);
+        String gptResult = openAiChatModel.call(prompt);
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            return mapper.readValue(gptResult, GptResponseDto.class);
         } catch (Exception e) {
-            throw new RuntimeException("GPT 응답 파싱 실패", e);
+            throw new CustomException(ErrorCode.INVALID_MAPPING_VALUE);
         }
+    }
+
+    public String makePrompt(String userRequest){
+        String prompt = GptPrompt.MCP_PROMPT;
+        return prompt.replace("${user_request}",userRequest);
     }
 }
