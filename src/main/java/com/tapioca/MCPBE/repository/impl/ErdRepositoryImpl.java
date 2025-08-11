7d7902c5bfbb240cbdcb764e4200717917eed9c4
Cho Suhyeon <ckc020506@gmail.com>
2025-08-11 20:21:14 +0900

    [feat] ErdRepoitory 추가

---
+++ src/main/java/com/tapioca/MCPBE/repository/impl/ErdRepositoryImpl.java
@@ -0,0 +1,19 @@
+package com.tapioca.MCPBE.repository.impl;
+
+import com.tapioca.MCPBE.domain.entity.erd.ErdEntity;
+import com.tapioca.MCPBE.repository.jpa.ErdJpaRepository;
+import com.tapioca.MCPBE.repository.repo.ErdRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ErdRepositoryImpl implements ErdRepository {
+
+    private final ErdJpaRepository erdJpaRepository;
+
+    @Override
+    public ErdEntity findByTeamEntity_code(String teamCode) {
+        return erdJpaRepository.findByTeamEntity_code(teamCode);
+    }
+}
